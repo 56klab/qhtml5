67e1b2da0d471aadab4a95a79e7a312bc319ba88
Simone Bussoni <info@simbus82.net>
2016-09-15 10:10:37 +0200

    Rimosso GA, aggiornato 2016

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
  * @package      Qhtml5
  *
  * @author       Quantility
- * @copyright    Copyright (C) 2015. All rights reserved.
+ * @copyright    Copyright (C) 2016. All rights reserved.
  * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 // no direct access
@@ -16,7 +16,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 	minimum-scale=<?php echo $this->params->get('minimum-scale')?>,
 	maximum-scale=<?php echo $this->params->get('maximum-scale')?>,
 	user-scalable=<?php echo $this->params->get('user-scalable')?> "/>
-
 <?php if($this->params->get('handheldfriendly') == 1) { ?>
 <meta name="HandheldFriendly" content="true" />
 <?php } ?>
@@ -24,7 +23,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <meta name="apple-mobile-web-app-capable" content="YES" />
 <?php } ?>
 <jdoc:include type="head" />
-
 <?php if($this->params->get('tpdebug') == 1): ?>
 <style>
 	* {border: 1px solid #f00;}
@@ -32,16 +30,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 </style>
 <?php endif; ?>
 <!--[if lt IE 9]><script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script><![endif]-->
-<?php
-	if($this->params->get('enablega') == 1) {
-	echo $this->params->get('scriptga');
-	}
-?>
-<?php
-$qhtml5_template = 'templates/' . $this->template . '/css/template.css';
-$qhtml5_magento = 'templates/' . $this->template . '/css/magento.css';
-$qhtml5_responsive = 'templates/' . $this->template . '/css/responsive.css';
-?>
 <link rel="stylesheet" type="text/css" href="<?php echo $qhtml5_template; ?>">
 <?php if(file_exists($qhtml5_magento)){ ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $qhtml5_magento; ?>">
