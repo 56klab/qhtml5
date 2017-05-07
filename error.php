d9c1cae3b3e326c69059a0088c085ef43186af57
Simone Bussoni <simbus82@gmail.com>
2017-05-07 17:45:42 +0200

    New 404
    Test - inclusione dell'intera struttura del template

--- error.php
+++ error.php
@@ -3,11 +3,10 @@
  * @package      Qhtml5
  *
  * @author       Quantility
- * @copyright    Copyright (C) 2015. All rights reserved.
+ * @copyright    Copyright (C) 2017. All rights reserved.
  * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
-header("HTTP/1.0 404 Not Found");
-echo file_get_contents(JURI::root().'/pagina-non-trovata');
+include "index.php";
 ?>
