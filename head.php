38d3df25258aa719f0a3bea9bd196753896bd20f
Simone Bussoni <simbus82@gmail.com>
2017-05-11 01:53:16 +0200

    Update head.php

--- head.php
+++ head.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * @package      Qhtml5
- *
+ * @subpackage   Templates.qhtml5
  * @author       Quantility
- * @copyright    Copyright (C) 2016. All rights reserved.
- * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * @copyright    Copyright (C) 2017. All rights reserved.
+ * @license      GNU General Public License version 2 or later; see LICENSE.txt
  */
 // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die;
 ?>
-<?php header('X-UA-Compatible: IE=edge,chrome=1');?>
+<?php header('X-UA-Compatible: IE=edge');?>
 <meta name="viewport" content="width=device-width,initial-scale=<?php echo $this->params->get('initial-scale')?>,minimum-scale=<?php echo $this->params->get('minimum-scale')?>,maximum-scale=<?php echo $this->params->get('maximum-scale')?>,user-scalable=<?php echo $this->params->get('user-scalable')?>"/>
 <?php if($this->params->get('handheldfriendly') == 1) { ?>
 <meta name="HandheldFriendly" content="true" />
@@ -25,7 +25,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 	*:hover {border: 1px solid #4218f8;}
 </style>
 <?php endif; ?>
-<!--[if lt IE 9]><script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script><![endif]-->
 <link rel="stylesheet" type="text/css" href="<?php echo $qhtml5_template; ?>">
 <?php if(file_exists($qhtml5_magento)){ ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $qhtml5_magento; ?>">
