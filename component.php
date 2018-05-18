3fcaa210bc60c0d1b81386f3545438bf3d5c01dc
Simone Bussoni <simbus82@gmail.com>
2018-05-18 12:10:43 +0200

    Alignment Joomla 3.8.x + 2018

--- component.php
+++ component.php
@@ -3,35 +3,37 @@
  * @package      Qhtml5
  *
  * @author       Quantility
- * @copyright    Copyright (C) 2017. All rights reserved.
+ * @copyright    Copyright (C) 2018. All rights reserved.
  * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL
  */
 defined('_JEXEC') or die;
 
-$app             = JFactory::getApplication();
-$doc             = JFactory::getDocument();
-$this->language  = $doc->language;
-$this->direction = $doc->direction;
+/** @var JDocumentHtml $this */
+$app = JFactory::getApplication();
+
+// Output as HTML5
+$this->setHtml5(true);
 
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 
+// Add html5 shiv
+JHtml::_('script', 'jui/html5.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
+
 // Add Stylesheets
-$doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
+JHtml::_('stylesheet', 'template.css', array('version' => 'auto', 'relative' => true));
 
 // Load optional rtl Bootstrap css and Bootstrap bugfixes
 JHtmlBootstrap::loadCss($includeMaincss = false, $this->direction);
 
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-<jdoc:include type="head" />
-<!--[if lt IE 9]>
-	<script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script>
-<![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<jdoc:include type="head" />
 </head>
-<body class="contentpane modal">
+<body class="contentpane modal<?php echo $this->direction === 'rtl' ? ' rtl' : ''; ?>">
 	<jdoc:include type="message" />
 	<jdoc:include type="component" />
 </body>
