dfe17703f79dbe71ddea44f2f6e5e6a7372a3df9
Simone Bussoni <simbus82@gmail.com>
2018-12-04 10:26:14 +0100

    Migliorato codice

--- error.php
+++ error.php
@@ -9,10 +9,12 @@
 // no direct access
 defined('_JEXEC') or die;
 header("HTTP/1.0 404 Not Found");
-$error_page = file_get_contents(JURI::root().'/pagina-non-trovata');
-if ( $error_page === false ) {
-  echo "Error 404 Page not Found";
+$file = JURI::root().'pagina-non-trovata';
+$file_headers = get_headers($file);
+
+if( (strpos($file_headers[0], '404') !== false) OR (strpos($file_headers[0], '508') !== false) ){
+    echo "Error 404 Page not Found";
 } else {
-  echo $error_page;
-};
+    echo file_get_contents($file);
+}
 ?>
