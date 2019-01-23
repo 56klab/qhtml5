3e2c83ea8ce93b591ad222e38677534b4c2fc64f
Simone Bussoni <simbus82@gmail.com>
2019-01-23 13:24:39 +0100

    Configurable alias for Page Not Found
    In template setting we can configure the alias used by menu voices for error Page (404). Es. page-not-found

--- error.php
+++ error.php
@@ -3,18 +3,22 @@
  * @package      Qhtml5
  * @subpackage   Templates.qhtml5
  * @author       Quantility
- * @copyright    Copyright (C) 2019. All rights reserved.
+ * @copyright    Copyright (C) 2017. All rights reserved.
  * @license      GNU General Public License version 2 or later; see LICENSE.txt
  */
 // no direct access
 defined('_JEXEC') or die;
 header("HTTP/1.0 404 Not Found");
 
-$langTag = JFactory::getLanguage()->getTag();
-$langUrl = substr($langTag, 0, -3);
+$langTag 		= JFactory::getLanguage()->getTag();
+$langUrl 		= substr($langTag, 0, -3);
 
-$file = JURI::root().$langUrl.'/pagina-non-trovata';
-$file_headers = get_headers($file);
+$app			= JFactory::getApplication();
+$params 		= $app->getTemplate(true)->params;
+$notfound_alias	= $params->get('notfound_alias');
+
+$file 			= JURI::root().$langUrl.'/'.$notfound_alias;
+$file_headers 	= get_headers($file);
 
 if( (strpos($file_headers[0], '404') !== false) OR (strpos($file_headers[0], '508') !== false) ){
     echo "Error 404 Page not Found";
