98182f39c0cb3e1430ae133dd53d938db249d531
Simone Bussoni <simbus82@gmail.com>
2019-01-23 13:10:41 +0100

    First multilanguage solution
    Create 2 menu voice with same "alias", one per languange. You can set different articles in different languages for the 404 page.
    
    Next step:
    1 - right code to get "url" language code from CMS
    2 - configurable 404 error page "alias"

--- error.php
+++ error.php
@@ -3,13 +3,17 @@
  * @package      Qhtml5
  * @subpackage   Templates.qhtml5
  * @author       Quantility
- * @copyright    Copyright (C) 2017. All rights reserved.
+ * @copyright    Copyright (C) 2019. All rights reserved.
  * @license      GNU General Public License version 2 or later; see LICENSE.txt
  */
 // no direct access
 defined('_JEXEC') or die;
 header("HTTP/1.0 404 Not Found");
-$file = JURI::root().'pagina-non-trovata';
+
+$langTag = JFactory::getLanguage()->getTag();
+$langUrl = substr($langTag, 0, -3);
+
+$file = JURI::root().$langUrl.'/pagina-non-trovata';
 $file_headers = get_headers($file);
 
 if( (strpos($file_headers[0], '404') !== false) OR (strpos($file_headers[0], '508') !== false) ){
