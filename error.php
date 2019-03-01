84142240dc8212378cdf43dac744c5dbb267ae57
Simone Bussoni <simbus82@gmail.com>
2019-03-01 12:40:27 +0100

    get rid of offline mode?

--- error.php
+++ error.php
@@ -10,19 +10,27 @@
 defined('_JEXEC') or die;
 header("HTTP/1.0 404 Not Found");
 
-$langTag 		= JFactory::getLanguage()->getTag();
-$langUrl 		= substr($langTag, 0, -3);
-
 $app			= JFactory::getApplication();
+$config         = JFactory::getConfig();
+$langTag 		= JFactory::getLanguage()->getTag();
+
 $params 		= $app->getTemplate(true)->params;
 $notfound_alias	= $params->get('notfound_alias');
 
+$langUrl 		= substr($langTag, 0, -3);
 $file 			= JURI::root().$langUrl.'/'.$notfound_alias;
+
 $file_headers 	= get_headers($file);
 
-if( (strpos($file_headers[0], '404') !== false) OR (strpos($file_headers[0], '508') !== false) ){
-    echo "Error 404 Page not Found";
+// joomla don't cal this error.php file when is offline, but i think is a bug... so...
+if( $config->get( 'offline' ) == 1 ) {
+    echo "Website is in Offline Mode: custome error 404 page works only in Online Mode";
 } else {
-    echo file_get_contents($file);
+
+    if( (strpos($file_headers[0], '404') !== false) OR (strpos($file_headers[0], '508') !== false) OR empty($notfound_alias) == true ){
+        echo "Error 404 Page not Found";
+    } else {
+        echo file_get_contents($file);
+    }
 }
 ?>
