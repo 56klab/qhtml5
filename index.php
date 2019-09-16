cbfbfbd1667d415e3f7e6a9770a150de3ba9d46a
Simone Bussoni <info@simbus82.net>
2019-09-17 01:23:46 +0200

    Big Update 09/2019
    First Attempt - Clean & Performance

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 // no direct access
 defined('_JEXEC') or die;
 
+// Caricamento classi da helpers/qhtml5.php
+JLoader::import('qhtml5.helpers.qhtml5', JPATH_THEMES);
+
 //Inizializzo le variabili per Joomla
 $app             = JFactory::getApplication();
 $user            = JFactory::getUser();
@@ -36,42 +39,62 @@ $contentwidth    	= '';
 if (is_object($menu)) {
     $pageclass = $menu->params->get('pageclass_sfx');
 }
+
+// ---------- caricamento CSS Styles ---------------- //
+
+if ($params->get('bootstrapcss') == '1') {
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css', false, false);
+    \JHtml::_('bootstrap.loadCss', false, $this->direction);
+} else if ($params->get('bootstrapcss') == '2') {
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css', false, false);
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap-responsive.min.css', false, false);
+    \JHtml::_('bootstrap.loadCss', false, $this->direction);
+} else if ($params->get('bootstrapcss') == '3') {
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css',false, false);
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap-responsive.min.css',false, false);
+    \JHtml::_('stylesheet', 'media/jui/css/bootstrap-extended.css',false, false);
+    \JHtml::_('bootstrap.loadCss', false, $this->direction);
+}
+
 $qhtml5_template	= 'templates/' . $this->template . '/css/template.css';
 $qhtml5_magento		= 'templates/' . $this->template . '/css/magento.css';
 $qhtml5_responsive	= 'templates/' . $this->template . '/css/responsive.css';
-if ($params->get('enable_honeypot') == 1) {
-$honeypot_file		= $params->get('honeypot_file');
-}
 
-if ($params->get('bootstrap') == '1') {
-	JHtml::_('bootstrap.framework');
-} else if ($params->get('bootstrap') == '2') {
-	// Bootstrap 3.x
-} else if ($params->get('bootstrap') == '3') {
-	// Bootstrap Latest 4.x
+if(file_exists($qhtml5_template)){
+	\JHtml::_('stylesheet', $qhtml5_template, false, false);
 }
-if ($params->get('bootstrapcss') == '1') {
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css',false,false);
-    JHtml::_('bootstrap.loadCss', false, $this->direction);
-} else if ($params->get('bootstrapcss') == '2') {
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css',false,false);
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap-responsive.min.css',false,false);
-    JHtml::_('bootstrap.loadCss', false, $this->direction);
-} else if ($params->get('bootstrapcss') == '3') {
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap.min.css',false,false);
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap-responsive.min.css',false,false);
-    JHtml::_('stylesheet', 'media/jui/css/bootstrap-extended.css',false,false);
-    JHtml::_('bootstrap.loadCss', false, $this->direction);
+if(file_exists($qhtml5_magento)){
+	\JHtml::_('stylesheet', $qhtml5_magento, false, false);
 }
+if(file_exists($qhtml5_responsive)){
+	\JHtml::_('stylesheet', $qhtml5_responsive, false, false);
+}
+// -------------------------------------------------- //
 
-// Add template js
-JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+
+// ---------- caricamento JS Scripts ---------------- //
 
 // Caricamento jquery UI core o sortable
+if($params->get('jquery') == 1):
+	\JHtml::_('jquery.framework');
+endif;
 if($params->get('jqueryui') == 1):
-	JHtml::_('jquery.ui', array('core', 'sortable'));
+	\JHtml::_('jquery.ui', array('core', 'sortable'));
 endif;
 
+if ($params->get('bootstrap') == '1') {
+	\JHtml::_('bootstrap.framework');
+} else if ($params->get('bootstrap') == '2') {
+	// Bootstrap Custom
+} 
+
+// Add template js
+\JHtml::_('script', 'template.js', array('version' => 'auto', 'relative' => true));
+
+// -------------------------------------------------- //
+
+
+// ---------- calcoli template ---------------- //
 //span per left e right: dimensioni fisse per migliore user interface span3 | span6 | span3
 if ($this -> countModules('left')) {
 	$leftspan = 'span3';
@@ -97,22 +120,29 @@ $bodyclass =	'site ' . $option . ' view-' . $view
 		. ($this->direction === 'rtl' ? ' rtl' : '');
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" prefix="og: http://ogp.me/ns#">
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-<?php
-	echo $params->get('after_head_open');
-	include 'head.php'; 								//include il file contente l'HEAD della pagina html
-	include 'files/favicon-app.php'; 						//include il file contente favicon e app icons
-	echo $params->get('before_head_close');
-?>
+	<!-- Metas -->
+	<meta charset="utf-8">
+	<?php echo $params->get('after_head_open'); ?>
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>
+	<?php echo $params->get('favicon_code');?>
+	<jdoc:include type="head" />
+
+	<!-- Styles -->
+
+	<!-- Scripts -->
+
+	<?php if ($this->countModules('head')): ?><jdoc:include type="modules" name="head" style="raw" /><?php } ?>
+	<?php echo $params->get('before_head_close');?>
 </head>
 <body class="<?php echo $bodyclass; ?>">
-<?php
-	echo $params->get('after_body_open');
-	include 'template.php';								//include la parte modificabile dalla sviluppatore del template
-	if ($params->get('enable_honeypot') == 1) { include 'files/honeypot.php'; }	//include honeypot
-	echo $params->get('before_body_close');
-?>
-<jdoc:include type="modules" name="debug" style="none" />
+	<?php echo $params->get('after_body_open'); ?>
+	<?php if ($params->get('browserupgrade') == '1') { ?>
+		<!--[if IE]><p class="browserupgrade">Stai usando un browser <strong>vecchio e non aggiornato</strong>. Per favore <a href="https://browsehappy.com/">aggiorna il tuo browser</a> per migliorare l'esperienza di navigazione e la sicurezza in ogni sito che visiterai.</p><![endif]-->
+	<?php } ?>
+	<?php include 'template.php'; ?>
+	<?php echo $params->get('before_body_close'); ?>
+	<jdoc:include type="modules" name="debug" style="none" />
 </body>
 </html>
--- template.php
+++ template.php
@@ -9,7 +9,6 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<!--[if IE]><p class="browserupgrade">Stai usando un browser <strong>vecchio e non aggiornato</strong>. Per favore <a href="https://browsehappy.com/">aggiorna il tuo browser</a> per migliorare l'esperienza di navigazione e la sicurezza in ogni sito che visiterai.</p><![endif]-->
 <section class="header">
 	<header class="container">
 		<div id="header" class="row">
